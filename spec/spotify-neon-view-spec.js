3eee0ee7b9b8ce370a4ba5a0d7632a7b7b2ada08
Matthew Hartman <msh1011@wildcats.unh.edu>
2018-05-18 13:42:23 -0400

    Initial Commit

---
+++ spec/spotify-neon-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SpotifyNeonView from '../lib/spotify-neon-view';
+
+describe('SpotifyNeonView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
