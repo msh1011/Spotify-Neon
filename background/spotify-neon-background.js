2ce2b3310e300819a2e3dbb28ddba4997c808345
Matthew Hartman <msh1011@wildcats.unh.edu>
2018-05-21 17:07:37 -0400

    added start of buttons

--- background/spotify-neon-background.js
+++ background/spotify-neon-background.js
@@ -8,9 +8,9 @@ export default class SpotifyNeonBackground {
 
   fetchData(){
     var access_token = atom.config.get("spotify-neon:access_token");
-    if(access_token == ""){
+    if(access_token == "" || access_token == undefined){
       return;
-    }
+    }  
     var playerUrl = 'https://api.spotify.com/v1/me/player';
     var playerHeader = { 
       method: 'GET',
@@ -26,9 +26,14 @@ export default class SpotifyNeonBackground {
         fetch("http://localhost:7888/refresh")
         return;
       }
+      // console.log(response)
       atom.config.set("spotify-neon:song_name", response.item.name);
       atom.config.set("spotify-neon:song_artist", response.item.artists[0].name);
       atom.config.set("spotify-neon:song_art_url", response.item.album.images[0].url);
+      if(!atom.config.get("spotify-neon:inform-outdated")){
+        atom.config.set("spotify-neon:song_isplaying", response.is_playing);
+      }
+      atom.config.set("spotify-neon:inform-outdated", false);
       atom.config.set("spotify-neon:song_progress", response.progress_ms);
       atom.config.set("spotify-neon:song_length", response.item.duration_ms);
     });
